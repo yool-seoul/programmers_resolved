dd423e6d22922e71c335ad82934e6a4e43ad7020
Sangyool Lee <sangyool.lee@sktair.com>
2021-08-27 11:42:44 +0900

    8/27 resolved

--- 12950.py
+++ 12950.py
@@ -1,11 +1,16 @@
 def solution(arr1, arr2):
-    arr3 = [i+j for i, j in zip(arr1, arr2)]
-
-    print(arr3)
+    arr3 = arr1.copy()
+    for i in range(len(arr1)):
+        for j in range(len(arr1[0])):
+            arr3[i][j] = arr1[i][j] + arr2[i][j]
     return arr3
 
 
-    
+# 한 줄 코딩
+def solution2(A, B):
+    return [[c + d for c, d in zip(a, b)] for a, b in zip(A,B)]
 
-print(solution([[1,2],[3,4]], [[3,4],[5,6]]) == [[4,6],[7,9]])
+
+print(solution([[1,2],[2,3]], [[3,4],[5,6]]) == [[4,6],[7,9]])
 print(solution([[1],[2]], [[3],[4]]) == [[4],[6]])
+
